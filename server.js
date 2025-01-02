e17912201a27b264fd520038335120b54051fa1c
lucas Loor <luca@loor.vc>
2025-01-02 19:44:28 -0300

    colocando a porta certa

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 'use strict'
 
+/*
+|--------------------------------------------------------------------------
+| Http server
+|--------------------------------------------------------------------------
+|
+| This file bootstrap Adonisjs to start the HTTP server. You are free to
+| customize the process of booting the http server.
+|
+| """ Loading ace commands """
+|     At times you may want to load ace commands when starting the HTTP server.
+|     Same can be done by chaining `loadCommands()` method after
+|
+| """ Preloading files """
+|     Also you can preload files by calling `preLoad('path/to/file')` method.
+|     Make sure to pass relative path from the project root.
+*/
+
 const { Ignitor } = require('@adonisjs/ignitor')
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  .fireHttpServer((handler) => {
-    // Use variáveis de ambiente ou valores padrão
-    const HOST = process.env.HOST || '0.0.0.0'
-    const PORT = process.env.PORT || 10000
-    return handler.listen(PORT, HOST) // Define porta e host explicitamente
-  })
+  .fireHttpServer()
   .catch(console.error)
