0d3f1eaac25862219aaebe9aa134a014d98a38c3
lucas Loor <luca@loor.vc>
2025-01-02 23:01:20 -0300

    colocando os nomes nas localizações

--- app/Controllers/Http/UserLocationController.js
+++ app/Controllers/Http/UserLocationController.js
@@ -1,27 +1,36 @@
 const UserLocation = use('App/Models/UserLocation');
+const User = use('App/Models/User');
 
 class UserLocationController {
 
-  async store ({ request, auth }) {
+  async store({ request, auth }) {
     const user = auth.user;
     const { latitude, longitude } = request.only(['latitude', 'longitude']);
+    
     const location = await UserLocation.create({
       user_id: user.id,
       latitude,
       longitude
     });
+    
     return location;
   }
 
   async index({ response }) {
     try {
-      // Buscando todas as localizações de todos os usuários
-      const locations = await UserLocation.all();
-      return response.json(locations);
+      // Buscando todas as localizações de todos os usuários, incluindo o nome do usuário e created_at
+      const locations = await UserLocation
+        .query()
+        .innerJoin('users', 'users.id', 'user_locations.user_id')  // Realizando o INNER JOIN
+        .select('user_locations.latitude', 'user_locations.longitude', 'user_locations.created_at', 'users.username')  // Selecionando os campos
+        .fetch();
+      
+      return response.json(locations);  // Retorna as localizações com as informações do usuário
     } catch (error) {
       return response.status(500).json({ error: 'Erro ao obter localizações' });
     }
   }
+  
 }
 
 module.exports = UserLocationController;
